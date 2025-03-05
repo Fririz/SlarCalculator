8aa5c4bd7d97f079b8c6859292dae0d9ba3f1a1d
Dmitriy <Fririzriz@gmail.com>
2025-03-05 15:20:39 +0200

    make +- buttons

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,26 +20,116 @@ namespace SlarCalculator
         public MainWindow()
         {
             InitializeComponent();
+
+
+
             ChangeSizeOfSLAR();
+
+        }
+
+        void PlusSize(object sender, RoutedEventArgs e)
+        {
+            if (size == 9)
+            {
+                //TODO сделать вывод ошибки
+            }
+            else
+            {
+                size += 1;
+                ChangeSizeOfSLAR();
+            }
+
+        }
+
+
+        void MinusSize(object sender, RoutedEventArgs e)
+        {
+            if (size == 1)
+            {
+                //TODO сделать вывод ошибки
+            }
+            else
+            {
+                size -= 1;
+                ChangeSizeOfSLAR();
+            }
 
         }
 
         void ChangeSizeOfSLAR()
         {
+            SLAR.Children.Clear();
+            SLAR.RowDefinitions.Clear();
+            SLAR.ColumnDefinitions.Clear();
             Grid myGrid = SLAR;
-            myGrid.ShowGridLines = true;
-            // Define the Columns
-            ColumnDefinition[] ArrayOfColumn = new ColumnDefinition[size];
-            for (int i = 0; i < size + 1; i++)
+            //myGrid.ShowGridLines = true;
+
+            // Определяем колонки
+            /*
+            for (int i = 0; i <= size * 2 + 2; i++)
             {
                 myGrid.ColumnDefinitions.Add(new ColumnDefinition());
             }
+
+            // Определяем строки
             for (int i = 0; i < size; i++)
             {
                 myGrid.RowDefinitions.Add(new RowDefinition());
             }
+            */
 
+            for (int i = 0; i < 20; i++)
+            {
+                myGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                myGrid.RowDefinitions.Add(new RowDefinition());
+            }
 
-        }
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j <= size * 2 + 2; j++)
+                {
+                    if (j % 2 == 0 && j < size * 2)
+                    {
+                        TextBox textbox = new TextBox();
+                        Grid.SetColumn(textbox, j);
+                        Grid.SetRow(textbox, i);
+                        myGrid.Children.Add(textbox);
+                    }
+                    else if (j % 2 != 0 && j < size * 2)
+                    {
+                        TextBlock myTextBlock = new TextBlock();
+                        myTextBlock.Text = $"x{(j + 1) / 2}";
+                        myTextBlock.VerticalAlignment = VerticalAlignment.Center;
+                        myTextBlock.HorizontalAlignment = HorizontalAlignment.Center;
+                        Grid.SetColumn(myTextBlock, j);
+                        Grid.SetRow(myTextBlock, i);
+                        myGrid.Children.Add(myTextBlock);
+                    }
+                    else if (j == size * 2)
+                    {
+                        TextBlock myTextBlock = new TextBlock();
+                        myTextBlock.Text = "=";
+                        myTextBlock.VerticalAlignment = VerticalAlignment.Center;
+                        myTextBlock.HorizontalAlignment = HorizontalAlignment.Center;
+                        Grid.SetColumn(myTextBlock, j);
+                        Grid.SetRow(myTextBlock, i);
+                        myGrid.Children.Add(myTextBlock);
+                    }
+                    else if (j == size * 2 + 1)
+                    {
+                        TextBox textbox = new TextBox();
+                        Grid.SetColumn(textbox, j);
+                        Grid.SetRow(textbox, i);
+                        myGrid.Children.Add(textbox);
+                    }
+                }
+            }
+
+            sizeOfMatrix.Text = $"Size: {size}";
+
+    }
     }
 }
